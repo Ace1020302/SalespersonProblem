04ba2f6bf8e8eb01d8346fffce43d3ff97f98169
phillipedwards <phillip11ace@gmail.com>
2023-11-30 20:01:34 -0500

    Added reading file functionality

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import webbrowser
 import networkx as nx
+import numpy
 from pyvis.network import Network
 import matplotlib.pyplot as plt
 
 
+def readNodes(fileName):
+    arr = []
 
+    file = open(fileName)
+    file.readline()
+    for line in file:
+        a, b = line.split()
+        a = int(a)
+        b = int(b)
+        arr.append((a, b))
+
+    file.close()
+    return arr
+
+
+def getDist(a, b):
+    numpy.hypot(a, b)
+
+
+
+nodes = readNodes("tsp_14.txt")
+# "tsp_14.txt"
+
+
+print(nodes)
 
 net = Network()
 
@@ -16,30 +41,32 @@ net = Network()
 # net.addEdge("B", "A", 5)
 
 
-
-g = nx.Graph()
-pos = nx.spring_layout(g, seed=3113794652)
-
-g.add_edge(1, 2)
-g.add_edge(2, 3)
-g.add_edge(3, 4)
-g.add_edge(1, 4)
-g.add_edge(1, 5)
-g.add_edge(3, 3)
-g.add_edge(6, 4)
-
-
-nx.draw_networkx(g, with_labels=True)
-
-
-net.from_nx(g)
+#
+# g = nx.Graph()
+# pos = nx.spring_layout(g, seed=3113794652)
+#
+# g.add_edge(1, 2)
+# g.add_edge(2, 3)
+# g.add_edge(3, 4)
+# g.add_edge(1, 4)
+# g.add_edge(1, 5)
+# g.add_edge(3, 3)
+# g.add_edge(6, 4)
+#
+#
+# nx.draw_networkx(g, with_labels=True)
+#
+#
+# net.from_nx(g)
 
 
 # plt.show()
-net.show("nx.html", notebook=False)
-
-
-webbrowser.open('http://localhost:63342/TravelingSalesperson/nx.html')
+# net.show("nx.html", notebook=False)
+#
+#
+# webbrowser.open('http://localhost:63342/TravelingSalesperson/nx.html')
 
 # plt.savefig("filename.png")
+
+
 
