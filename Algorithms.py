2bd593f3bfcc559271892a399f458240ea7ec08d
phillipedwards <phillip11ace@gmail.com>
2023-12-05 20:44:12 -0500

    MST Completed

--- Algorithms.py
+++ Algorithms.py
@@ -1,15 +1,17 @@
 # This is where we will do the algorithms
+import copy
+
 
 class Algorithms:
     def Naive(self, graph, nodes):
-        #sanity check
-        arr1 = [ (-193, 8782, 2), (-5168, 2636, 3), (-4521, 1266, 11), (-7005, 2118, 6),
-                 (-9860, 1311, 13), (-9955, -2923, 5), (-8022, -3864, 4), (-7795, -5000, 10), (-3138, -2512, 0),
-                 (7775, -8002, 7), (9478, -1973, 9), (6804, -1072, 1), (4244, -1339, 8), (-192, 3337, 12)]
-        print(self.permute_iterative(nodes, 0, 10, graph, 1000000)) #10 nodes hit
+        # sanity check
+        arr1 = [(-193, 8782, 2), (-5168, 2636, 3), (-4521, 1266, 11), (-7005, 2118, 6),
+                (-9860, 1311, 13), (-9955, -2923, 5), (-8022, -3864, 4), (-7795, -5000, 10), (-3138, -2512, 0),
+                (7775, -8002, 7), (9478, -1973, 9), (6804, -1072, 1), (4244, -1339, 8), (-192, 3337, 12)]
+        print(self.permute_iterative(nodes, 0, 10, graph, 1000000))  # 10 nodes hit
         pass
 
-    #Calculates the path of each individual node between another node
+    # Calculates the path of each individual node between another node
     def calculate_path_cost(self, arr, graph):
         sum = 0
         for i in range(1, len(arr)):
@@ -18,24 +20,25 @@ class Algorithms:
             sum += graph[first_point][second_point]
         return sum
 
-    #Every permutation of the possible nodes
+    # Every permutation of the possible nodes
     def permute_iterative(self, arr, l, r, graph, dist):
         import time
         import math
 
-        stack = [] #stack to hold the list of nodes and the first and final index
+        stack = []  # stack to hold the list of nodes and the first and final index
         stack.append((list(arr), l, r))
 
-        source = arr[0] #Keeping track of what our "source" node is
+        source = arr[0]  # Keeping track of what our "source" node is
         start = time.time()
         while stack:
             arr, l, r = stack.pop()
-            origDist = 0 #Reset original distance
+            origDist = 0  # Reset original distance
 
             if l == r:
                 if source != arr[0]:
                     source = arr[0]
-                    origDist = math.dist([0,0,0], arr[0]) #Calculating the distance from the origin to our first point
+                    origDist = math.dist([0, 0, 0],
+                                         arr[0])  # Calculating the distance from the origin to our first point
                     print(f'From Origin: {0, 0} + {arr[0]} = {origDist}')
                 newDist = origDist + self.calculate_path_cost(arr, graph)
                 print(f'Distance: {newDist}')
@@ -56,12 +59,13 @@ class Algorithms:
     def OptimialNaive(self, nodes):
         pass
 
-    def Approximation(self, edge_graph, nodes, start):
+    def Approximation(self, edgeGraph, nodes, start):
+        # These nodes are actually node objects!
         shortestPath = []
 
         # Create MST of G using Prims
         print("test")
-        mst_walk = self.MST_Prim(edge_graph, nodes, start)
+        mst_walk = self.MST_Prim(edgeGraph, nodes, start, len(nodes))
         print(mst_walk)
 
         preorder_walk = list(set(mst_walk))
@@ -70,60 +74,61 @@ class Algorithms:
 
         return shortestPath
 
+    def MST_Prim(self, edgeGraph, nodes, startNode, n):
+        # mst arr
+        mst = []
+        tmpUnchecked = nodes.copy()
+        s = startNode
+        for i in range(n):
+            if len(tmpUnchecked) == 1:
+                mst += tmpUnchecked
+                break
+
+            minEdge = self.findMinEdge(self.getEdgesFromGraph(edgeGraph, s, n, mst))
+            nextNode = minEdge.nodeB
+            mst.append(s)
+            # print(tmpUnchecked)
+            if(s in tmpUnchecked):
+                print(s.key, " in tmpUnchecked. Will be removed.")
+            else:
+                print(s.key, " in not tmpUnchecked. Will not be removed. Major Tom to ground control")
+            tmpUnchecked.remove(s)
+            s = nextNode
+            print(s.key, " is new node")
+            print("=" * 50)
 
 
-    def MST_Prim(self, edge_graph, nodes, start):
-        # Set of which nodes (keys) have been indexed. Node[2] = key of node
-        mstNodes = []
+        return mst
 
-        # Get the edges connected to the node minus self edges
-        edges = self.getEdgesFromGraph(start, nodes, edge_graph)
-
-        # Sets the current node to the start node
-        current_node = start
-
-        n = len(nodes)
-        for u in n:
-            minEdge = self.findMinEdge(edge_graph, current_node, edges)
-            mstNodes.append(current_node) # Adds key of the node to the searched list
-            for v in n:
-               notInSet = True
-               # mst[i] = (x, y, key) @ index i
-               # mst[i][2] = key at index
-               if(nodes[v] in mstNodes):
-                   notInSet = False
-
-               if (edge_graph[u][v] > 0) and (notInSet) and (start > edge_graph[u][v]):
-                   start = edge_graph[u][v]
-
-            current_node = minEdge # Grabs the node that the current node connects to?
-        return mstNodes
-
-
-    def findMinEdge(self, graph, node, edges):
+    def findMinEdge(self, edges):
         # Find minimum cost edge
         minEdge = edges[0]
 
         # Finding the min cost edge
-        for i in range(len(edges)):
-            if(graph[node[2]][minEdge[2]] > graph[node[2]][edges[i][2]]):
+        for i in range(1, len(edges)):
+
+            # if edge at index i is less than the current minimum edge then the min edge should be replaced
+            if edges[i].distance < minEdge.distance:
                 minEdge = edges[i]
 
         return minEdge
 
-
-    def getEdgesFromGraph(self, node, nodes, graph):
+    # Returns all edges stemming from given node -- Except for self-edge (when edge distance == 0)
+    def getEdgesFromGraph(self, edgeGraph, node, n, exclusionList):
         # arr of nodes that have an edge to the given node
         arr = []
-        node_key = node[2]
-        for i in range(len(graph[node_key])):
-            # Avoid Self Edges
-            if(i == node_key):
-                continue
-            else:
-                arr.append(nodes[i])
-        return arr
 
+        # Returns all edges stemming from given node -- Except for self-edge (when edge distance == 0)
+        for v in range(n):
+            checkingEdge = edgeGraph[node.key][v]
+            print(checkingEdge.nodeA.key, checkingEdge.nodeB.key)
+            # edge distance != 0
+            if (checkingEdge.distance != 0 and (checkingEdge.nodeB not in exclusionList)):
+                print("Appended the Edge")
+                arr.append(checkingEdge)
+            else:
+                print("Did not append")
+        return arr
 
     def GreedyBound(self, nodes):
         pass
--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def readNewNodes(fileName, skipFirstLine=True):
         a, b = line.split()
         a = int(a)
         b = int(b)
-        node = Node.Node()
+        node = Node(None, None, None)
         node.coords = (a, b)
         node.key = key # Index needed to access element in our graph
         arr.append(node)
@@ -114,7 +114,7 @@ def printGraph(graph, labels):
         graph[i].remove(labels[i])
 
 
-def compute_edge_graph(self, nodes):
+def compute_edge_graph(nodes):
     # Store the graph with the distances. This is really an adjacency matrix (2D array)
     # Set up adjacency matrix dimensions, square matrix len(nodes) by len(nodes)
     nodeCount = len(nodes)
@@ -129,10 +129,11 @@ def compute_edge_graph(self, nodes):
             if (adj_mat[i][j] != 0):
                 continue
             # Calculate value once for each node.
-            distance = self.getDist(nodes[i], nodes[j])
-            edge = Edge.Edge(nodes[i], nodes[j], distance)
+            distance = getDist(nodes[i].coords, nodes[j].coords)
+            edge = Edge(nodes[i], nodes[j], distance)
             adj_mat[i][j] = edge
-            adj_mat[j][i] = edge
+            mirroredEdge = Edge(nodes[j], nodes[i], distance)
+            adj_mat[j][i] = mirroredEdge
             # Message to clarify distance calculation per-step after getDist
             # print(
                 # f"compute_graph :: Distance between Node {i} {nodes[i]} and Node {j} {nodes[j]} = {adj_mat[i][j]}")
@@ -155,7 +156,6 @@ def run():
 
     graph = (compute_graph(nodes))
     edge_graph = compute_edge_graph(new_nodes)
-    print(graph[nodes[12][2]][nodes[13][2]]) #This accesses whichever node we want in the adj_mat
 
     # Add Nodes to network
     for i in range(len(nodes)):
