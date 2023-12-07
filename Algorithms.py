ddb94bf3578eba77e8760655bf80c2a2c5e62346
SelfJ19 <SELFJ1@ETSU.EDU>
2023-12-07 16:24:27 -0500

    stuffs

--- Algorithms.py
+++ Algorithms.py
@@ -12,12 +12,7 @@ import networkx as nx
 class Algorithms:
     #Naive Brute Force Algo
     def Naive(self, graph, nodes):
-        print(self.permute_iterative(nodes, 0, 5, graph, 1000000))
-        # sanity check
-        arr1 = [(-193, 8782, 2), (-5168, 2636, 3), (-4521, 1266, 11), (-7005, 2118, 6),
-                (-9860, 1311, 13), (-9955, -2923, 5), (-8022, -3864, 4), (-7795, -5000, 10), (-3138, -2512, 0),
-                (7775, -8002, 7), (9478, -1973, 9), (6804, -1072, 1), (4244, -1339, 8), (-192, 3337, 12)]
-        print(self.permute_iterative(nodes, 0, 10, graph, 1000000))  # 10 nodes hit
+        print(self.permute_iterative(nodes, 0, 10, graph, 1000000), file=open('Results.txt', 'a'))  # 10 nodes hit
         pass
 
     # Calculates the path of each individual node between another node
@@ -45,12 +40,8 @@ class Algorithms:
                     source = arr[0]
                     origDist = math.dist([0, 0, 0], arr[0])  # Calculating the distance from the origin to our first point
                 newDist = origDist + self.calculate_path_cost(arr, graph)
-                #print(f'Distance: {newDist}')
-                #time.sleep(5)
                 if dist > newDist:
                     dist = newDist
-                    #print(f'New Shortest Distance: {dist}')
-                #print(arr)
             else:
                 for i in range(l, r):
                     arr[l], arr[i] = arr[i], arr[l] #Swaps two nodes in the path
@@ -88,8 +79,8 @@ class Algorithms:
         with multiprocessing.Pool() as pool:
             all_paths = pool.map(self.permute_optimized, nodes)  # Allocates processes to every processor core in PC, returns a list of all shortest paths
 
-        print(f'Shortest Distance: {min(all_paths)}')  # Takes the shortest of the shortest paths
-        print(time.time() - start)
+        print(f'Shortest Distance: {min(all_paths)}', file=open('Results.txt', 'a'))  # Takes the shortest of the shortest paths
+        print(time.time() - start, file=open('Results', 'a'))
 
     # MST and standard pre-order walk approach without any external library
     # prints the cost of the approximated path
