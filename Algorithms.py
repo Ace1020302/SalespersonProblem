b3d37a80ba143fea3bdf2da54b671a1f6dc31fdd
iStratix <itsnagle@gmail.com>
2023-12-07 00:03:53 -0500

    Fixed error where head node was not removed from optimized brute force

--- Algorithms.py
+++ Algorithms.py
@@ -17,7 +17,7 @@ class Algorithms:
         arr1 = [(-193, 8782, 2), (-5168, 2636, 3), (-4521, 1266, 11), (-7005, 2118, 6),
                 (-9860, 1311, 13), (-9955, -2923, 5), (-8022, -3864, 4), (-7795, -5000, 10), (-3138, -2512, 0),
                 (7775, -8002, 7), (9478, -1973, 9), (6804, -1072, 1), (4244, -1339, 8), (-192, 3337, 12)]
-        print(self.permute_iterative(nodes, 0, 10, graph, 1000000))  # 10 nodes hit
+        print(self.permute_iterative(nodes, 0, 11, graph, 1000000))  # 10 nodes hit
         pass
 
     # Calculates the path of each individual node between another node
@@ -48,29 +48,31 @@ class Algorithms:
                     source = arr[0]
                     origDist = math.dist([0, 0, 0],
                                          arr[0])  # Calculating the distance from the origin to our first point
-                    print(f'From Origin: {0, 0} + {arr[0]} = {origDist}')
+                    #print(f'From Origin: {0, 0} + {arr[0]} = {origDist}')
                 newDist = origDist + self.calculate_path_cost(arr, graph)
-                print(f'Distance: {newDist}')
-                time.sleep(5)
+                #print(f'Distance: {newDist}')
+                #time.sleep(5)
                 if dist > newDist:
                     dist = newDist
-                    print(f'New Shortest Distance: {dist}')
-                print(arr)
+                    #print(f'New Shortest Distance: {dist}')
+                #print(arr)
             else:
                 for i in range(l, r):
                     arr[l], arr[i] = arr[i], arr[l]
                     stack.append((list(arr), l + 1, r))
                     arr[l], arr[i] = arr[i], arr[l]
         finalToOrigin = math.dist(arr[r], [0, 0, 0])
-        print(f'Final Node To Origin: {arr[r]} + {[0, 0]} = {finalToOrigin}')
+        #print(f'Final Node To Origin: {arr[r]} + {[0, 0]} = {finalToOrigin}')
         return time.time() - start, dist + finalToOrigin
 
     #TODO: Continue research on checking if there is a faster way to implement multiprocessing, this implementation is slower than naive somehow
     def permute_optimized(self, head):
-        remainingNodes = set(readNodes('tsp_14.txt')) - set(head)  # Removes the head from the set of nodes to permute
+        nodes = set(readNodes('tsp_14.txt'))
+        nodes.remove(head)
+        remainingNodes = nodes
         shortestDist = 10000000000
         dist = None  # Distance at the end of the loop
-        for tail in permutations(remainingNodes, 4):  # Tracks for every permutation of the remaining nodes
+        for tail in permutations(remainingNodes, 11):  # Tracks for every permutation of the remaining nodes
             if head in tail:  # Ensures no self-edges included
                 continue
             distTrack = math.dist([0, 0, 0], head)
@@ -81,8 +83,8 @@ class Algorithms:
                 else:
                     dist = distTrack + math.dist(tail[i - 1], tail[i])
                     distTrack = dist
-            if dist < shortestDist:  # If our final distance is shorter than a previous calculated distance, replace it
-                shortestDist = dist
+            if math.dist(tail[-1], [0, 0, 0]) + dist < shortestDist:  # If our final distance is shorter than a previous calculated distance, replace it
+                shortestDist = math.dist(tail[-1], [0,0,0]) + dist
                 # print(f'Head: {head}\nTail: {tail}')
                 # print(f'Distance: {shortestDist}')
         return shortestDist
